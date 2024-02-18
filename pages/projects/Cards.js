a5f584dcdb0ed41c9d370b091d77c6b1e18526f7
Sahar Mirkamrani <saharmirkamrani@gmail.com>
2024-02-18 15:02:39 +0330

    chore: Resume update

--- pages/projects/Cards.js
+++ pages/projects/Cards.js
@@ -1,9 +1,38 @@
 import React from "react";
 import Link from "next/link";
-import YouTube from 'react-youtube';
+import YouTube from "react-youtube";
 
 const Cards = () => {
   const projects = [
+    {
+      id: 1,
+      title: "Nitroprop",
+      technologies: [
+        "Reactjs",
+        "Nextjs",
+        "AntD",
+        "Context API",
+        "scss and global css variables",
+        "Formik",
+      ],
+      description:
+        "This project is a prop trading firm consist of a landing and an admin dashboard, and I was responsible for maintaining both projects for months as the only front-end developer in the team.",
+      url: "https://nitroprop.com",
+    },
+    {
+      id: 1,
+      title: "Hunters",
+      technologies: [
+        "Typescript",
+        "Styles components",
+        "NextAuth",
+        "Redux",
+        "React Query",
+      ],
+      description:
+        "This is a Nextjs project with a large codebase. This app is about learning fasting and healthy eating in order to get in shape and it is owned by a well-known influencer in the field of nutrition and diet. I worked with a fullstack developer in the team to add more features and resolve the existing bugs and also refactor some parts in this project.",
+      url: "https://hunter-s.com",
+    },
     {
       id: 1,
       title: "ETCBAYC",
@@ -18,7 +47,7 @@ const Cards = () => {
       ],
       description:
         "This is a large scale Nextjs project and it is a minting website for the well-known NFT collection, ETC Bored Ape Yacht Club. It includes merch shopping functionality as well as info about the project roadmap",
-      url: "https://etcbayc.com/",
+      url: "Unfortunately this project is down",
     },
     {
       id: 2,
@@ -33,22 +62,13 @@ const Cards = () => {
       ],
       description:
         "ETCBillboard is a 100x100 and 1st Rentable NFT Project on Ethereum Classic (ETC) blockchain, where people can advertise and promote anything by showing any picture on their billboards.",
-      url: "https://etcbillboard.com/",
-    },
-    {
-      id: 3,
-      title: "NFT Marketplace",
-      videoURL: "GeUmFT9pFHI",
-      technologies: ["Style components", "React.js"],
-      description:
-        "This is a complete project of an NFT marketplace with modern and minimalistic design. It contains separate pages for every NFT a bidding section and a simple pretty landing page as well as individual pages for every NFT creator.",
-      url: "https://yt-react-next-ui-crypto-nft.vercel.app/",
+      url: "Unfortunately this project is down",
     },
   ];
 
-	const onPlayerReady = (event) => {
+  const onPlayerReady = (event) => {
     event.target.pauseVideo();
-  }
+  };
 
   const opts = {
     height: "250",
@@ -70,9 +90,9 @@ const Cards = () => {
               key={project.id}
               className="w-full md:w-1/3 lg:max-w-max p-4 transform hover:scale-105 transition-transform duration-300"
             >
-              <div className="rounded-lg shadow-lg overflow-hidden bg-teal-800 flex flex-col items-center pt-4 md:min-h-full">
+              <div className="rounded-lg shadow-lg overflow-hidden bg-teal-700 flex flex-col items-center pt-4 md:min-h-full">
                 {/* Video */}
-                <YouTube videoId={project.videoURL} opts={opts} onReady={onPlayerReady} />
+                {project.videoURL ? (<YouTube videoId={project.videoURL} opts={opts} onReady={onPlayerReady} />): null}
                 {/* Project Details */}
                 <div className="p-4">
                   <h3 className="text-xl font-semibold mb-2">
@@ -89,12 +109,16 @@ const Cards = () => {
                   <p>{project.description}</p>
                   <div className="font-bold text-red-300 my-4">
                     Link:{" "}
-                    <Link href={project.url} target="_blank">
-                      {" "}
-                      <span className="cursor-pointer text-cream-100">
-                        {project.title}{" "}
-                      </span>{" "}
-                    </Link>
+                    {project.url.includes("down") ? (
+                      <p>{project.url}</p>
+                    ) : (
+                      <Link href={project.url} target="_blank">
+                        {" "}
+                        <span className="cursor-pointer text-cream-100">
+                          {project.title}{" "}
+                        </span>{" "}
+                      </Link>
+                    )}
                   </div>
                 </div>
               </div>
@@ -103,11 +127,21 @@ const Cards = () => {
         </div>
       </div>
 
-			<div className="flex justify-center items-center mx-2">
-      <div className="bg-red-300 text-cream-100 p-4 rounded-md text-center">
-        To view my other projects such as <span className="font-semibold">a real estate website</span> and more check out my <Link className="underline" href="https://github.com/SaharMirkamrani" target="_blank">GitHub</Link>! 😊 
-      </div>
-    </div>
+      {/* <div className="flex justify-center items-center mx-2">
+        <div className="bg-red-300 text-cream-100 p-4 rounded-md text-center">
+          To view my other projects such as{" "}
+          <span className="font-semibold">a real estate website</span> and more
+          check out my{" "}
+          <Link
+            className="underline"
+            href="https://github.com/SaharMirkamrani"
+            target="_blank"
+          >
+            GitHub
+          </Link>
+          ! 😊
+        </div>
+      </div> */}
     </div>
   );
 };
